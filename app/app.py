fc65909d0ec4f67ad0cc13d98dc529adbe6542d5
jakobrobert <jakob-robert@hotmail.de>
2021-04-20 21:58:08 +0200

    Create database and show todos in list

--- app/app.py
+++ app/app.py
@@ -1,12 +1,26 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+
+# TODO extract into separate file
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255))
+    completed = db.Column(db.Boolean)
 
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    todos = Todo.query.all()
+    return render_template("index.html", todos=todos)
 
 
 if __name__ == "__main__":
+    db.create_all()
     app.run(debug=True)

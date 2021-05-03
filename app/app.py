3bbd436ad3558d5b1c2061271ed1f54d407292fc
jakobrobert <jakob-robert@hotmail.de>
2021-05-03 21:27:56 +0200

    Move constants URL_PREFIX and DATABASE_URI into server.ini config file, add this config file to .gitignore

--- app/app.py
+++ app/app.py
@@ -1,12 +1,15 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
+import configparser
 
-URL_PREFIX = "/dev/todo-app"
-
+config = configparser.ConfigParser()
+config.read("server.ini")
+URL_PREFIX = config["DEFAULT"]["URL_PREFIX"]
+DATABASE_URI = config["DEFAULT"]["DATABASE_URI"]
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 

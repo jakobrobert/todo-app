77934c394b034da9627e0ffbffc3e1124e03fe53
jakobrobert <jakob-robert@hotmail.de>
2022-10-09 14:48:12 +0200

    Copy function __find_todos_for_date to class LongTermTodoOverview

--- app/logic/long_term_todo_overview.py
+++ app/logic/long_term_todo_overview.py
@@ -11,3 +11,18 @@ class LongTermTodoOverview:
             all_dates.append(curr_date)
 
         return all_dates
+
+    @staticmethod
+    def find_todos_for_date(todos, date):
+        todos_for_date = []
+
+        for todo in todos:
+            if todo.timestamp_completed is None:
+                continue
+
+            todo_date = todo.timestamp_completed.date()
+            if todo_date == date:
+                todos_for_date.append(todo)
+
+        return todos_for_date
+

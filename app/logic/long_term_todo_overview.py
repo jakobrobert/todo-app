cc36ed56c83cbb46c30266f717441ae0d7a9d3ea
jakobrobert <jakob-robert@hotmail.de>
2022-10-09 14:41:29 +0200

    Copy function collect_dates_of_todos to class LongTermTodoOverview

--- app/logic/long_term_todo_overview.py
+++ app/logic/long_term_todo_overview.py
@@ -1,2 +1,13 @@
 class LongTermTodoOverview:
-    pass
+    @staticmethod
+    def collect_dates_of_todos(todos):
+        all_dates = []
+
+        for todo in todos:
+            if todo.timestamp_completed is None:
+                continue
+
+            curr_date = todo.timestamp_completed.date()
+            all_dates.append(curr_date)
+
+        return all_dates

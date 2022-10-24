a7d4686dfe7406862e7d5fb8cfa50172976438c9
jakobrobert <jakob-robert@hotmail.de>
2022-10-24 13:04:39 +0200

    Fix TypeError: Add None check in Utils which has been removed in previous commit 773388675cd726826a62f16c33c1ebffaf743c26

--- app/utils.py
+++ app/utils.py
@@ -1,6 +1,10 @@
 class Utils:
     @staticmethod
     def calculate_progress_in_percents(progress, progress_goal):
+        # This check is needed, otherwise TypeError can occur
+        if progress is None or progress_goal is None:
+            return
+
         progress_in_percents = 100.0 * progress / progress_goal
         return Utils.round_decimal(progress_in_percents)
 

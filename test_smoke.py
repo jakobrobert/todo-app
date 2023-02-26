9dc27c202c06e0be4c9ebacae9c08fc5f9442c03
jakobrobert <jakob-robert@hotmail.de>
2023-02-26 22:09:19 +0100

    Remove obsolete TODOs. Tests now use real database, is not optimal, but okay for a start. They read the database uri from server.ini as well.

--- test_smoke.py
+++ test_smoke.py
@@ -3,18 +3,18 @@
 import unittest
 
 from todo_app import app
+from todo_app import URL_PREFIX
 
 
 class TestSmoke(unittest.TestCase):
     def setUp(self):
         app.config["TESTING"] = True
         app.config["DEBUG"] = True
-        # TODO set database uri
         self.app = app.test_client()
 
     def tearDown(self):
         pass
 
     def test_index(self):
-        response = self.app.get("/", follow_redirects=True)
+        response = self.app.get(f"{URL_PREFIX}/", follow_redirects=True)
         self.assertEqual(response.status_code, 200)

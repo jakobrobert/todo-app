bb746da54eea1d75f2143032d6e08c1f48bbe070
jakobrobert <jakob-robert@hotmail.de>
2023-03-01 08:42:50 +0100

    #150 Fix KeyError 'progress_as_percents' which can occur in progress-overview if 'As percents' option is enabled (#157)
    * Add test to reproduce the bug. Seems to occur always, easy to test
    
    * Fix code to make test, use "progress_as_percents" consistently instead of "progress_in_percents"
    
    * Change "daily_progress_in_percents" as well to "daily_progress_as_percents"
    
    * Fix templates as well, those are not covered by smoke tests.
    
    It just showed empty string for progress in %, led to no exception
    
    * Adjust column names in progress overview table

--- core/logic/long_term_todo_overview.py
+++ core/logic/long_term_todo_overview.py
@@ -75,7 +75,7 @@ class LongTermTodoOverview:
             labels.append(item["date"])
 
             if as_percents:
-                values.append(item["progress_in_percents"])
+                values.append(item["progress_as_percents"])
             else:
                 values.append(item["progress"])
 
@@ -270,28 +270,28 @@ class LongTermTodoOverview:
     def __fill_item_for_progress_overview(self, curr_item, prev_item, progress):
         if progress == 0:
             curr_item["daily_progress"] = 0
-            curr_item["daily_progress_in_percents"] = 0
+            curr_item["daily_progress_as_percents"] = 0
 
             if prev_item is None:
                 curr_item["progress"] = 0
-                curr_item["progress_in_percents"] = 0
+                curr_item["progress_as_percents"] = 0
 
                 return
 
             curr_item["progress"] = prev_item["progress"]
-            curr_item["progress_in_percents"] = prev_item["progress_in_percents"]
+            curr_item["progress_as_percents"] = prev_item["progress_as_percents"]
 
             return
 
         curr_item["progress"] = progress
-        curr_item["progress_in_percents"] = Utils.convert_to_percents(progress, self.progress_goal)
+        curr_item["progress_as_percents"] = Utils.convert_to_percents(progress, self.progress_goal)
 
         relative_progress = progress
         if prev_item is not None:
             relative_progress -= prev_item["progress"]
 
         curr_item["daily_progress"] = relative_progress
-        curr_item["daily_progress_in_percents"] = Utils.convert_to_percents(relative_progress, self.progress_goal)
+        curr_item["daily_progress_as_percents"] = Utils.convert_to_percents(relative_progress, self.progress_goal)
 
     @staticmethod
     def __get_active_days_count_by_date_and_todos_mapping(date_and_todos_mapping):

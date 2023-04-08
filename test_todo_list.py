4a3ce9ea1778403d851a75a7f1f941c950d455b8
jakobrobert <jakob-robert@hotmail.de>
2023-04-08 13:12:47 +0200

    Add test_delete_todo_list

--- test_todo_list.py
+++ test_todo_list.py
@@ -46,6 +46,7 @@ class TestTodoList(unittest.TestCase):
 
     def test_edit_todo_list_title(self):
         old_title = "Old Title"
+        # TODO CLEANUP instead use TodoList.add, need to adjust method so returns the id
         todo_list = TodoList(title=old_title)
         db.session.add(todo_list)
         db.session.commit()
@@ -62,8 +63,23 @@ class TestTodoList(unittest.TestCase):
         self.assertEqual(updated_todo_list.title, new_title)
 
     def test_delete_todo_list(self):
-        # TODO
-        pass
+        # TODO CLEANUP instead use TodoList.add, need to adjust method so returns the id
+        todo_list = TodoList()
+        db.session.add(todo_list)
+        db.session.commit()
+
+        todo_list_id = todo_list.id
+
+        found_todo_list = TodoList.get(todo_list_id)
+        self.assertTrue(found_todo_list is not None)
+
+        # TODO CLEANUP should be DELETE method, NOT GET. Will change in #48
+        url = f"{self.url_prefix}/{todo_list_id}/delete"
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 302)
+
+        found_todo_list = TodoList.get(todo_list_id)
+        self.assertTrue(found_todo_list is None)
 
     def test_add_todo(self):
         # TODO
